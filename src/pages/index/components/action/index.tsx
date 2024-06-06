b151a4bc732caba846c02e0e5683117e7ec4075a
Onzeng <2528358258@qq.com>
2024-06-06 16:12:33 +0800

    优化

--- src/pages/index/components/action/index.tsx
+++ src/pages/index/components/action/index.tsx
@@ -1,25 +1,26 @@
-import './index.css'
-import React from "react";
-import {ListItemProps} from './type.ts'
+import './index.css';
+import React from 'react';
+import { ListItemProps } from './type.ts';
 
 export default function action(props: {
-    List: ListItemProps[],
-    setList: React.Dispatch<React.SetStateAction<ListItemProps[]>>;
+  List: ListItemProps[];
+  setList: React.Dispatch<React.SetStateAction<ListItemProps[]>>;
 }) {
-    const {List, setList} = props;
-    // 添加
-    const handleAdd = () => {
-        const newItem = {
-            name: "",
-            isEdit: false,
-            id: Math.random(),
-        };
-        setList([...List, newItem]);
+  const { List, setList } = props;
+  
+  // 添加
+  const handleAdd = () => {
+    const newItem = {
+      name: '',
+      isEdit: false,
+      id: Math.random(),
     };
+    setList([...List, newItem]);
+  };
 
-    return (
-        <div className='box2'>
-            <button onClick={handleAdd}>添加</button>
-        </div>
-    )
+  return (
+    <div className='box2'>
+      <button onClick={handleAdd}>添加</button>
+    </div>
+  );
 }
--- src/pages/index/components/listItem/type.ts
+++ src/pages/index/components/listItem/type.ts
@@ -4,8 +4,8 @@
  * @param {String} name 名称
  * @param {boolean} isEdit 编辑状态
  */
-export interface ListItemProps {
-    id: number;
-    name: string;
-    isEdit: boolean;
-}
+export type ListItemProps = {
+  id: number;
+  name: string;
+  isEdit: boolean;
+};
